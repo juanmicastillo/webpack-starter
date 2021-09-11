609b6d5f67def49ad3f3157441bf148e48b8a002
juanmicastillo <juanmicastillo@gmail.com>
2021-09-11 18:35:25 +0200

    primer commit

---
+++ src/detectar.js
@@ -0,0 +1,10 @@
+console.log(navigator.userAgent);
+console.log(navigator.vendor);
+console.log(navigator.hardwareConcurrency);
+console.log(navigator.language);
+console.log(navigator.languages);
+
+
+
+
+
